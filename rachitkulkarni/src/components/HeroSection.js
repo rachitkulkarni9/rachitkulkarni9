c33adc5c3da77c19f1a16cc163055e493eb96c3b
rkulka38 <rkulka38@asu.edu>
2024-12-23 17:41:21 -0700

    updated buttons
    Signed-off-by: rkulka38 <rkulka38@asu.edu>

--- rachitkulkarni/src/components/HeroSection.js
+++ rachitkulkarni/src/components/HeroSection.js
@@ -11,17 +11,16 @@ const HeroSection = () => {
             <h1>Rachit Girish Kulkarni</h1>
             <h2>Full Stack Developer <span>🚀</span></h2>
             <div className="buttons">
-            <a
-              href="/Resume.pdf" /* Path to your resume in the public folder */
-              download
-              className="download-cv"
-            >
-              <span>📄</span> Download CV
-            </a>
-            <button className="contact-info">
-              <span>📞</span> Contact Info
-            </button>
-          </div>
+                <a
+                    href="/resume.pdf"
+                    download
+                    className="download-cv"
+                >
+                    <span>📄</span> Download CV
+                </a>
+                </div>
+
+
             <div className="social-icons">
               <a href="https://www.linkedin.com/in/rachitkulkarni/" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-linkedin"></i>
